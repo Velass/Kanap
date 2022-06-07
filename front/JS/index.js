18ede954efbbf430d86577435cd0ed0bb55b545c
florian <furnari.florian@hotmail.fr>
2022-06-07 18:05:52 +0200

    page produit effectuée

--- front/JS/index.js
+++ front/JS/index.js
@@ -1,31 +1,36 @@
-console.log("charger")
 
 const items = document.getElementById("items")
-const createa = document.createElement("a")
-function fetchProduct(){
+
+function fetchProduct() {
     fetch('http://localhost:3000/api/products')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        for (let index = 0; index < 7; index++) {
-            const id = data[index]._id;
-            const imageUrl = "test " + data[index].imageUrl;
-            console.log(imageUrl)
-            
-        }
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            for (let index = 0; index < data.length; index++) {
+                items.innerHTML = items.innerHTML+ `
+                <a href=./product.html?id=${data[index]._id}>
+                   <article>
+                     <img src=${data[index].imageUrl} alt=${data[index].altTxt}>
+                     <h3 class="productName">${data[index].name}</h3>
+                     <p class="productDescription">${data[index].description}</p>
+                   </article>
+                </a> `;
+            }
+        })
 
 }
+// let result = await fetch('http://localhost:3000/api/products') 
+//result.json let data = result.json
 
 
-function loadProduct(){
+function loadProduct() {
 
     // recup de l'element id items
-    fetchProduct()
+    fetchProduct();
     //recup de fetch(http://localhost:3000/api/products) 
     //boucle for sur le resultat fetch
-    // dans la boucle generer une chaine de caractere qui contient le html des articles
-    // assigner dans le inerhtml de items la valeurs que je recupere (la chaine de caracterre)
+    // dans la boucle generer une chaine de caractere qui contient le html des datas
+    // assigner dans le inerhtml drre)e items la valeurs que je recupere (la chaine de caracte
 
 };
 
--- front/JS/script.js
+++
@@ -1,3 +0,0 @@
-console.log("charger")
-
-function loadProduct()
