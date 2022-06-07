fb6f6743483226541cd70c044d8a1a16c6cbdadb
florian <furnari.florian@hotmail.fr>
2022-06-07 18:43:17 +0200

    modif sur le index (enlever les console.log etc)

--- front/JS/index.js
+++ front/JS/index.js
@@ -5,7 +5,6 @@ function fetchProduct() {
     fetch('http://localhost:3000/api/products')
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             for (let index = 0; index < data.length; index++) {
                 items.innerHTML = items.innerHTML+ `
                 <a href=./product.html?id=${data[index]._id}>
@@ -19,18 +18,10 @@ function fetchProduct() {
         })
 
 }
-// let result = await fetch('http://localhost:3000/api/products') 
-//result.json let data = result.json
-
 
 function loadProduct() {
 
-    // recup de l'element id items
     fetchProduct();
-    //recup de fetch(http://localhost:3000/api/products) 
-    //boucle for sur le resultat fetch
-    // dans la boucle generer une chaine de caractere qui contient le html des datas
-    // assigner dans le inerhtml drre)e items la valeurs que je recupere (la chaine de caracte
 
 };
 
