457723fded74291bdf4b8ac48a9e32bff257e641
florian <furnari.florian@hotmail.fr>
2022-06-13 18:38:32 +0200

    commencement du clickevent

--- front/JS/product.js
+++ front/JS/product.js
@@ -4,10 +4,11 @@ const url = new URLSearchParams(windowsSearch)
 const id = url.get("id")
 
 const itemImg = document.getElementsByClassName("item__img")[0];
-const title = document.getElementById("title")
-const price = document.getElementById("price")
-const description = document.getElementById("description")
-const optionColor = document.getElementById("colors")
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const optionColor = document.getElementById("colors");
+const addCart = document.getElementById("addToCart");
 
 
 
@@ -15,7 +16,6 @@ function fetchProduct() {
     fetch(`http://localhost:3000/api/products/${id}`)
         .then(response => response.json())
         .then(data => product(data))
-        
 };
 
 function product(data) {
@@ -26,14 +26,35 @@ function product(data) {
     title.innerHTML = data.name
     price.innerHTML = data.price
     description.innerHTML = data.description
+    for (let color of data.colors) {
+        const createOption = document.createElement("option")
+        optionColor.appendChild(createOption)
+        createOption.value = color
+        createOption.innerHTML = color
+    };
 
-
-    console.log(data)
 };
 
 
+
+function cart() {
+
+    addCart.addEventListener("click", (event) => {
+        const color = optionColor.value;
+        const quantity = document.getElementById("quantity").value;
+        console.log(id)
+        console.log(color)
+        console.log(quantity)
+
+
+    })
+
+};
+
 function loadProduct() {
-    fetchProduct()
-}
+    fetchProduct(),
+    cart();
+
+};
 
 window.onload = loadProduct
